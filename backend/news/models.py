4d84319bbb4a7a0291b181e22264201ee27e177e
rahathosen <rahathosen.me@gmail.com>
2023-07-19 12:16:18 +0600

    init

---
+++ backend/news/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.CharField(max_length=255)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
